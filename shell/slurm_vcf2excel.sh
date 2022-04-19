3ff62907b24947fd33ade152e894bc9b7b669d4e
Ellen Schofield <ellen@pawprintsdesign.co.uk>
2022-04-19 14:18:16 +0100

    Update slurm_vcf2excel.sh

--- shell/slurm_vcf2excel.sh
+++ shell/slurm_vcf2excel.sh
@@ -2,6 +2,9 @@
 
 ## Command to Run :  bash slurm_vcf2excel.sh <Disease Status File> <CHR>
 
+
+SCRIPTS=`dirname $0`
+
 #VCF_FILE='/rds/project/rds-Qr3fy2NTCy0/Data/ens_WGS_219_VEP.vcf.gz'
 DISEASE_STATUS=$1
 #CHR=$2
@@ -47,31 +50,4 @@ if [ $count != 41 ]; then
 fi
 
 dos2unix ${DISEASE_STATUS}
-
-sbatch <<EOT
-#!/bin/bash
-
-#SBATCH -A MELLERSH-SL3-CPU
-#SBATCH -J vcf2excel
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --time 02:00:00
-#SBATCH --mail-type=ALL
-#SBATCH -p skylake
-#SBATCH --array 1-41
-
-#SBATCH -o $HOME/rds/hpc-work/logs/job-%A_%a.out
-
-module purge                               # Removes all modules still loaded
-module load rhel7/default-peta4            # REQUIRED - loads the basic environment
-
-
-CHR=`cut -f 1 -d':' ${FASTA}/${REF}-genomicsDB.intervals | cut -f 1 -d' ' | cut -d'_' -f 1 | sort -n | uniq | head -\${SLURM_ARRAY_TASK_ID} | tail -1`
-
-#perl /rds/project/rds-Qr3fy2NTCy0/Software/Git/Scripts/perl/vcf2excel.pl --default --vcf ${VCF_FILE} --status_file ${DISEASE_STATUS}
-#perl /rds/project/rds-Qr3fy2NTCy0/Software/Git/Scripts/perl/vcf2excel.pl --default --vcf ${VCF}/vcf_chr/ens_WGS_219-chr\${SLURM_ARRAY_TASK_ID}.vcf.gz --status_file ${DISEASE_STATUS}
-
-echo "perl /rds/project/rds-Qr3fy2NTCy0/Software/Git/Scripts/perl/vcf2excel.pl --default --vcf ${VCF_DIR}/${REF}-chr\${CHR}.ann.vcf.gz --status_file ${DISEASE_STATUS}"
-perl /rds/project/rds-Qr3fy2NTCy0/Software/Git/Scripts/perl/vcf2excel.pl --default --vcf ${VCF_DIR}/${REF}-chr\${CHR}.ann.vcf.gz --status_file ${DISEASE_STATUS}
-EOT
-
+sbatch --export=DISEASE_STATUS=${DISEASE_STATUS},FASTA=${FASTA},VCF_DIR=${VCF_DIR},SCRIPTS=${SCRIPTS},REF=${REF} ${SCRIPTS}/slurm/vcf2excel.sh;
