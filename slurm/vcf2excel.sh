a0c18e0353fda310dd0a3d1396650dc99d58e3be
Ellen Schofield <es904@cam.ac.uk>
2022-04-20 09:36:53 +0100

    lcoation of slurm script changed

--- slurm/vcf2excel.sh
+++ slurm/vcf2excel.sh
@@ -9,7 +9,8 @@
 #SBATCH -p skylake
 #SBATCH --array 1-41
 
-#SBATCH -o $HOME/rds/hpc-work/logs/job-%A_%a.out
+##SBATCH -o $HOME/rds/hpc-work/logs/job-%A_%a.out
+#SBATCH -o job-%A_%a.out
 
 module purge                               # Removes all modules still loaded
 module load rhel7/default-peta4            # REQUIRED - loads the basic environment
@@ -22,5 +23,5 @@ CHR=`cut -f 1 -d':' ${FASTA}/${REF}-genomicsDB.intervals | cut -f 1 -d' ' | cut 
 #perl /rds/project/rds-Qr3fy2NTCy0/Software/Git/Scripts/perl/vcf2excel.pl --default --vcf ${VCF_FILE} --status_file ${DISEASE_STATUS}
 #perl /rds/project/rds-Qr3fy2NTCy0/Software/Git/Scripts/perl/vcf2excel.pl --default --vcf ${VCF}/vcf_chr/ens_WGS_219-chr\${SLURM_ARRAY_TASK_ID}.vcf.gz --status_file ${DISEASE_STATUS}
 
-echo "perl ${SCRIPTS}/perl/vcf2excel.pl --default --vcf ${VCF_DIR}/${REF}-chr${CHR}.ann.vcf.gz --status_file ${DISEASE_STATUS}"
-perl ${SCRIPTS}/perl/vcf2excel.pl --default --vcf ${VCF_DIR}/${REF}-chr${CHR}.ann.vcf.gz --status_file ${DISEASE_STATUS}
+echo "perl ${SCRIPTS}/../perl/vcf2excel.pl --default --vcf ${VCF_DIR}/${REF}-chr${CHR}.ann.vcf.gz --status_file ${DISEASE_STATUS}"
+perl ${SCRIPTS}/../perl/vcf2excel.pl --default --vcf ${VCF_DIR}/${REF}-chr${CHR}.ann.vcf.gz --status_file ${DISEASE_STATUS}
