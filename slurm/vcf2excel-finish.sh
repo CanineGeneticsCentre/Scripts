41e2849040ae79eae6973ebda81b65e909f715e2
Ellen Schofield <es904@cam.ac.uk>
2023-09-20 11:04:29 +0100

    modified the live versions

--- slurm/vcf2excel-finish.sh
+++ slurm/vcf2excel-finish.sh
@@ -6,15 +6,18 @@
 #SBATCH --ntasks=1
 #SBATCH --time 00:05:00
 #SBATCH --mail-type=ALL
-#SBATCH -p skylake
+#SBATCH -p cclake
 
 #SBATCH -o logs/job-%j.out
 
+. /etc/profile.d/modules.sh                 # Leave this line (enables the module command)
 module purge                               # Removes all modules still loaded
 module load rhel7/default-peta4            # REQUIRED - loads the basic environment
 
-samples=`wc -l ${DISEASE_STATUS} | cut -f1 -d' '`
-col=`expr 2 \* $samples + 14`
+#samples=`wc -l ${DISEASE_STATUS} | cut -f1 -d' '`
+samples=`grep -vi 'omit' ${DISEASE_STATUS} | wc -l | tr -d ' '`
+col_effect=`expr 2 \* $samples + 14`
+col_seg=`expr 2 \* $samples + 4`
 
 file=`ls *_filtered_and_sorted_for_excel.txt | head -1`
 head -1 $file > header.txt
@@ -24,9 +27,9 @@ mv *_filtered_and_sorted_for_excel.txt *_vcf2excel_missing_effects.out *_vcf2exc
 
 
 for i in `seq 1 5`; do
-  (cat header.txt; awk -F"\t" -v c=$col -v seg=$i '($c == seg)' output/*_filtered_and_sorted_for_excel.txt) > vcf2excel-seg${i}.txt
-  LINECOUNT=`wc -l vcf2excel-seg${i}.txt | cut -f1 -d' '`
+  (cat header.txt; awk -F"\t" -v c=$col_effect -v effect=$i '($c == effect)' output/*_filtered_and_sorted_for_excel.txt) > vcf2excel-effect${i}.txt
+  LINECOUNT=`wc -l vcf2excel-effect${i}.txt | cut -f1 -d' '`
   if [[ $LINECOUNT == 1 ]]; then
-    rm -f vcf2excel-seg${i}.txt
+    rm -f vcf2excel-effect${i}.txt
   fi
 done
--- slurm/vcf2excel.sh
+++ slurm/vcf2excel.sh
@@ -6,18 +6,23 @@
 #SBATCH --ntasks=1
 #SBATCH --time 02:00:00
 #SBATCH --mail-type=ALL
-#SBATCH -p skylake
-#SBATCH --array 1-41
+#SBATCH -p cclake
+##SBATCH --array 1-41
 
 #SBATCH -o logs/job-%A_%a.out
 
-module purge                               # Removes all modules still loaded
-module load rhel7/default-peta4            # REQUIRED - loads the basic environment
+. /etc/profile.d/modules.sh                 # Leave this line (enables the module command)
+module purge                                # Removes all modules still loaded
+module load rhel7/default-peta4             # REQUIRED - loads the basic environment
 
 # Exported variables...
 # DISEASE_STATUS, FASTA, VCF_DIR, SCRIPTS, REF
 
 CHR=`cut -f 1 -d':' ${FASTA}/${REF}-genomicsDB.intervals | cut -f 1 -d' ' | cut -d'_' -f 1 | sort -n | uniq | head -${SLURM_ARRAY_TASK_ID} | tail -1`
 
-echo "perl ${SCRIPTS}/../perl/vcf2excel.pl --default --vcf ${VCF_DIR}/${REF}-chr${CHR}.ann.vcf.gz --status_file ${DISEASE_STATUS}"
-perl ${SCRIPTS}/../perl/vcf2excel.pl --default --vcf ${VCF_DIR}/${REF}-chr${CHR}.ann.vcf.gz --status_file ${DISEASE_STATUS}
+if [[ ${#CHR} -lt 4 ]] ; then
+  CHR="chr"${CHR}
+fi
+
+echo "perl ${SCRIPTS}/../perl/vcf2excel.pl --default --vcf ${VCF_DIR}/${REF}-${CHR}.ann.vcf.gz --status_file ${DISEASE_STATUS}"
+perl ${SCRIPTS}/../perl/vcf2excel.pl --default --vcf ${VCF_DIR}/${REF}-${CHR}.ann.vcf.gz --status_file ${DISEASE_STATUS}
