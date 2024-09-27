105960990fe63fb693ad221a377e663b6045353e
Ellen Schofield <10760087+tottlefields@users.noreply.github.com>
2024-09-27 14:27:45 +0100

    Update vcf2excel.sh

--- slurm/vcf2excel.sh
+++ slurm/vcf2excel.sh
@@ -15,10 +15,7 @@ module purge                               # Removes all modules still loaded
 module load rhel8/default-ccl              # REQUIRED - loads the basic environment
 
 # Exported variables...
-# DISEASE_STATUS, VCF_DIR, SCRIPTS, BASE, CONFIG
-
-REF="cf4"
-source ${CONFIG}
+# DISEASE_STATUS, VCF_DIR, SCRIPTS, REF, CONFIG
 
 CHR=`cut -f 1 -d':' ${FASTA}/${REF}-genomicsDB.intervals | cut -f 1 -d' ' | cut -d'_' -f 1 | uniq | head -${SLURM_ARRAY_TASK_ID} | tail -1`
 
@@ -26,5 +23,5 @@ if [[ ${#CHR} -lt 4 ]] ; then
   CHR="chr"${CHR}
 fi
 
-echo "perl ${SCRIPTS}/../perl/vcf2excel.pl --default --vcf ${VCF_DIR}/${BASE}-${CHR}.ann.vcf.gz --status_file ${DISEASE_STATUS}"
-perl ${SCRIPTS}/../perl/vcf2excel.pl --default --vcf ${VCF_DIR}/${BASE}-${CHR}.ann.vcf.gz --status_file ${DISEASE_STATUS}
+echo "perl ${SCRIPTS}/../perl/vcf2excel.pl --default --vcf ${VCF_DIR}/${REF}-${CHR}.ann.vcf.gz --status_file ${DISEASE_STATUS}"
+perl ${SCRIPTS}/../perl/vcf2excel.pl --default --vcf ${VCF_DIR}/${REF}-${CHR}.ann.vcf.gz --status_file ${DISEASE_STATUS}
